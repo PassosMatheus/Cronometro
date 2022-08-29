176cafd2df3584886f291a274727abfe8aa8c80c
Martins Passos <matheus.passos@externos-br.cencosud.com>
2022-08-29 16:22:01 -0300

    Implementado opção de contagem entre segundos e minutos

--- Program.cs
+++ Program.cs
@@ -7,12 +7,26 @@ namespace Stopwath
     {
         static void Main(string[] args)
         {
-            Start();
+            Menu();
         }
 
-        static void Start()
+        static void Menu()
         {
-            int time = 10;
+            Console.Clear();
+            Console.WriteLine("Digite o tempo que deseja cronometrar: ");
+            int seconds = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Deseja contar em segundos ou minutos? ");
+            Console.WriteLine("S = Segundos");
+            Console.WriteLine("M = Minutos");
+            string data = Console.ReadLine().ToLower();
+
+            Verify(data, seconds);
+
+        }
+
+        static void Start(int time)
+        {
             int currentTime = 0;
 
             while (currentTime != time)
@@ -21,6 +35,27 @@ namespace Stopwath
                 currentTime++;
                 Console.WriteLine(currentTime);
                 Thread.Sleep(1000);
+            }
+        }
+
+        static void Verify(string data, int seconds)
+        {
+            int multiplier = 60;
+            if (data == "m")
+            {
+                int num = seconds * multiplier;
+                Start(num);
+                Menu();
+            }
+            else if (data == "s")
+            {
+                Start(seconds);
+                Menu();
+            }
+            else
+            {
+                Console.WriteLine("Comando inválido, saindo...");
+                System.Environment.Exit(0);
             }
         }
     }
